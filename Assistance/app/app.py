7e88c40b7cf113a99ed66c67e11aa86b52225f62
oshiooshi <shiori.4211@gmail.com>
2021-09-03 13:59:58 +0900

    update file

--- Assistance/app/app.py
+++ Assistance/app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask import redirect,url_for
 
 app = Flask(__name__)
 
@@ -6,6 +7,18 @@ app = Flask(__name__)
 @app.route("/index")
 def index():
     return render_template("index.html")
+
+@app.route("/index", methods=["post"])
+def translate():
+    #翻訳ボタンを押した際の処理
+    if "translate" in request.form:
+        code = request.form["code"]
+        return render_template("index.html", code=code, transcode=code)
+
+    #評価ボタンを押した際の処理
+    elif "good" in request.form:
+        transcode = request.form["transcode"]
+        return render_template("index.html", code=transcode, transcode=transcode)
 
 
 
